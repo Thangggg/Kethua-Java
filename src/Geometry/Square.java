b7cb08d873e6a7a9ec770c8865d0bfd0fd1d91b5
Thangggg <thang.nguyenvan7594@gmail.com>
2021-11-11 15:54:50 +0700

    Point2d_3d

--- src/Geometry/Square.java
+++ src/Geometry/Square.java
@@ -1,13 +1,16 @@
 package Geometry;
 
 public class Square extends Shape {
-
+    private double side = 1.0;
     public Square() {
     }
 
-    public Square(String color, boolean filled) {
-        super(color, filled);
+    public Square(double side) {
+        this.side = side;
     }
 
-
+    public Square(String color, boolean filled, double side) {
+        super(color, filled);
+        this.side = side;
+    }
 }
---
+++ src/Point2D_3D/Point3D.java
@@ -0,0 +1,40 @@
+package Point2D_3D;
+
+public class Point3D extends Point2D {
+    private float z = 1.0f;
+
+    public Point3D() {
+
+    }
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public void setXYZ(float x, float y, float z){
+        setXY(x, y);
+        this.z = z;
+    }
+
+    public float[] getXYZ() {
+        float [] array = new float[3];
+        array[0] = getX();
+        array[1] = getY();
+        array[2] = this.z;
+        return array;
+    }
+
+    @Override
+    public String toString(){
+        return " ( " +getX()+ " , " +getY()+ " , " +getZ()+ " ) ";
+    }
+}
